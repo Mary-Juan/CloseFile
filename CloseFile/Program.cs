016031fb82ff31b183d7e8352531f6f496af954f
mobocomputer <mobocomputer@TEST-SYSTEM>
2023-10-30 13:09:25 -0700

    File will close finally.

--- CloseFile/Program.cs
+++ CloseFile/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Test1()
+{
+    string filePath = @"c:\iman.txt";
+    FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+    var txt = fs.ReadByte();
+
+    fs.Close();
+
+}
+
+ void Test2()
+{
+    string filePath = @"c:\iman.txt";
+    FileStream fs = null;
+    try
+    {
+        fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        var txt = fs.ReadByte();
+    }
+    finally
+    {
+        fs?.Close();
+    }
+}
